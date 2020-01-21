336a645ad33085372982a673fbe641d4305de6dc
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-01-21 05:25:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay(musicians, instruments)
   for(var i = 0; i < musicians.length; i++)
   {
     var sentence = musicians[i] + " plays " + instruments[i]
-    whoplayswhat.add(sentence)
+    whoplayswhat.push(sentence)
   }
 }
